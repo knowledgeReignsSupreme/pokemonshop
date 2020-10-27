eb236253c10cc66fcdce877323f9e5274caab048
david <david17895@gmail.com>
2020-10-27 20:46:29 +0200

    Updates

---
+++ precache-manifest.bd3da018eeee6f51fd0ee024b498ca3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d56247b79b9cd337c5c70c7975658a0b",
+    "url": "/pokemonshop/index.html"
+  },
+  {
+    "revision": "cbbd1d25a60ffb1bf2e2",
+    "url": "/pokemonshop/static/css/main.4f292904.chunk.css"
+  },
+  {
+    "revision": "89b17b4d364c8195be78",
+    "url": "/pokemonshop/static/js/2.f24baf76.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/pokemonshop/static/js/2.f24baf76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbbd1d25a60ffb1bf2e2",
+    "url": "/pokemonshop/static/js/main.3a3390e1.chunk.js"
+  },
+  {
+    "revision": "69fc57d11a04845ea24e",
+    "url": "/pokemonshop/static/js/runtime-main.850edef0.js"
+  }
+]);
